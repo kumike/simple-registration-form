6eb38d4ea65ce4634898579712b0860b2e7191e2
kumike <michailkubrak@gmail.com>
2018-07-14 02:33:07 +0300

    renew pdodb.php

--- pdodb.php
+++ pdodb.php
@@ -1,6 +1,6 @@
 <?php
-$db_host = 'localhost';
-$db_name = 'regPass';
-$db_user = 'mishele';
-$db_pass = '1437';
+$db_host = '';
+$db_name = '';
+$db_user = '';
+$db_pass = '';
 $dbh = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_pass);
